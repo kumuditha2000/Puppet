6ad4d198e1874300b2d593d9b0247da3dec794e7
kumuditha2000 <kumudithaudaya@gmail.com>
2018-10-25 20:55:12 +0530

    Pword

--- Register.php
+++ Register.php
@@ -3,7 +3,7 @@ $con = mysqi_connect("127.0.0.1:3306","puppet_root", "root123", "sack_web_puppet
 
 $Name = $_POST["Name"];
 $Phone = $_POST["Phone"]
-$Password = $_POST["Password"];
+$Password = $_POST["Pword"];
 
 $statement = mysqli_prepare($con, "INSERT INTO user (Name, Phone, Password) VALUES (?, ?, ?)");
 mysqli_stmt_bind_param($statement, "siss", $Name, $Phone, $Password);
